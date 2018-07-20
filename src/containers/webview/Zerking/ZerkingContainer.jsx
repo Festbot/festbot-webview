87f539be6997095d50e8511a029689d39800d01a
analoguezone <info@analoguezone.com>
2018-07-20 16:03:07 +0200

    add google maps, and position

--- src/containers/webview/Zerking/ZerkingContainer.jsx
+++ src/containers/webview/Zerking/ZerkingContainer.jsx
@@ -1,63 +1,75 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import classes from './ZerkingContainer.css'
+import classes from './ZerkingContainer.css';
+import { Helmet } from 'react-helmet';
+
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 const geo_options = {
-  enableHighAccuracy: true, 
-  maximumAge        : 30000, 
-  timeout           : 27000
+	enableHighAccuracy: true,
+	maximumAge: 30000,
+	timeout: 27000
+};
+const mapContainer = {
+	width: '100%',
+	height: '200px'
 };
 
 export class ZerkingContainer extends Component {
-  state={
-    lat:'',
-    lng:''
-  }
+	state = {
+		lat: '',
+		lng: ''
+	};
 
-  componentDidMount(){
+	componentDidMount() {
+		const watchID = navigator.geolocation.watchPosition(this.geo_success, this.geo_error, geo_options);
+	}
 
-    const watchID = navigator.geolocation.watchPosition(this.geo_success, this.geo_error, geo_options);
+	componentWillUnmount() {
+		navigator.geolocation.clearWatch(this.watchID);
+	}
 
-  }
+	geo_success = position => {
+		this.setState({
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		});
+	};
 
-  componentWillUnmount(){
-    navigator.geolocation.clearWatch(this.watchID);
-  }
-
-   geo_success = (position) => {
-    this.setState({
-      lat:position.coords.latitude, lng:position.coords.longitude
-    }) 
-  }
-  
-   geo_error(err) {
-   console.log(err)
-    alert("error:",err.message);
-  }
-  
-
-  
-
-
-  render() {
+	geo_error(err) {
+		alert('error:', err.message);
+	}
 
 
 
-    return (
-      <div>
-        Location: {this.state.lat} lng:{this.state.lng}
+	render() {
+		return (
+			<div>
+				<Helmet>
+					<title>Zerking</title>
+				</Helmet>
+        {this.state.lat && (
+					<Map style={mapContainer} google={this.props.google} center={{ lat: this.state.lat, lng: this.state.lng }} zoom={17}>
+						<Marker name={'Your position'} position={{ lat: this.state.lat, lng: this.state.lng }} />
+						<Marker />
+					</Map>
+        )}
+        
+		
+        
+        <div className={classes.container}>
+        
+        <div className={classes.button}>{`Add this`}</div>
+        
+        
+        
+        </div>
 
-         {this.state.lat&&
-          <div className="classes.mapContainer">
-          <div>
-          </div>
-          <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.lat},${this.state.lng}&zoom=18&size=300x300&sensor=false&key=AIzaSyDTCkVlGg_KCM_MH7WM3Yd-gXlhPhaWvFw`} />
-          </div>
-        } 
-
-      </div>
-    )
-  }
+			</div>
+		);
+	}
 }
 
-export default ZerkingContainer
+export default GoogleApiWrapper({
+	apiKey: 'AIzaSyDTCkVlGg_KCM_MH7WM3Yd-gXlhPhaWvFw'
+})(ZerkingContainer);
