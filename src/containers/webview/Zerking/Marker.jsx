4a252bbe3ee7a1d0ee44594688dcb9a33cbab9fe
andormade <hngrhorace@gmail.com>
2018-07-23 10:05:15 +0200

    fix marker removal

--- src/containers/webview/Zerking/Marker.jsx
+++ src/containers/webview/Zerking/Marker.jsx
@@ -1,63 +1,56 @@
-import React, { Component } from 'react'
-import {Consumer} from './Map.jsx'
-var marker=''
+import React, { Component } from 'react';
+import { Consumer } from './Map.jsx';
 
 const iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-  
+
 const icons = {
-parking: {
-  icon: iconBase + 'parking_lot_maps.png'
-},
-library: {
-  icon: iconBase + 'library_maps.png'
-},
-info: {
-  icon: iconBase + 'info-i_maps.png'
-},
-arrow: {
-  icon: iconBase + 'cross-hairs_highlight.png'
-},
-orange_pin: {
-  icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_orange.png'
-}
+	parking: {
+		icon: iconBase + 'parking_lot_maps.png'
+	},
+	library: {
+		icon: iconBase + 'library_maps.png'
+	},
+	info: {
+		icon: iconBase + 'info-i_maps.png'
+	},
+	arrow: {
+		icon: iconBase + 'cross-hairs_highlight.png'
+	},
+	orange_pin: {
+		icon: 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_orange.png'
+	}
 };
 
 export class Marker extends Component {
+	componentWillUnmount() {
+		if (this.marker) {
+			this.marker.setMap(null);
+		}
+	}
 
-  componentWillUnmount(){
+	setMapMarker = (map, google) => {
+		const { pos, iconType = 'orange_pin' } = this.props;
 
-  //  if (!marker=='') {marker.setMap(null)}
-    //while(markersArray.length) { markersArray.pop().setMap(null); }
-  //  marker=''
-  }
+		this.marker = new google.maps.Marker({
+			position: pos,
+			map: map,
+			icon: icons[iconType].icon
+		});
 
-  setMapMarker=(map,google)=>{
+		console.log('[mapsMARKER]', this.marker);
+	};
 
-    const {pos,iconType="orange_pin"} = this.props
-
-     marker = new google.maps.Marker({
-      position: pos,
-      map: map,
-      icon:icons[iconType].icon
-    });
-
-    //markersArray.push(marker)
-    console.log("[mapsMARKER]",marker)
-  }
-
-  render() {
-    const {pos,iconType="info"} = this.props
-
-    return (
-        
-        <Consumer>
-        {({map,google})=>{if (map&&iconType) {
-          this.setMapMarker(map,google)
-        }}}
-        </Consumer>
-
-    )
-  }
+	render() {
+		return (
+			<Consumer>
+				{({ map, google }) => {
+					if (map) {
+						this.setMapMarker(map, google);
+					}
+				}}
+			</Consumer>
+		);
+	}
 }
 
-export default Marker
+export default Marker;
