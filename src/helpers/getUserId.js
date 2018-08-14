7e2a68d721a01007c9c008ec5c88c186c4887ee7
analoguezone <info@analoguezone.com>
2018-08-15 00:24:40 +0200

    remove alert

--- src/helpers/getUserId.js
+++ src/helpers/getUserId.js
@@ -1,6 +1,6 @@
 import md5 from 'md5';
-import store from '../store/store.js'
-import {setIsWebview} from '../store/actions'
+import store from '../store/store.js';
+import { setIsWebview } from '../store/actions';
 
 export default function getUserId() {
 	return new Promise((complete, reject) => {
@@ -8,19 +8,17 @@ export default function getUserId() {
 			MessengerExtensions.getContext(
 				'817793415088295',
 				({ psid }) => {
-						console.warn("[PSID OK]",psid)
-						complete(md5(psid))
-						store.dispatch(setIsWebview())
+					console.warn('[PSID OK]', psid);
+					complete(md5(psid));
+					store.dispatch(setIsWebview());
 				},
 				err => {
 					console.warn('no psid :(');
-					const isDev = document.cookie.indexOf("festbotDebug")>-1
+					const isDev = document.cookie.indexOf('festbotDebug') > -1;
 					if (isDev) {
-						alert("User data error, please reload the page, teszt user dev")
 						complete('4842567782cdf2aa620f1060e1dcbcd7');
 					} else {
-						alert("User data error, please reload the page,teszt user")
-						reject()
+						reject();
 					}
 				}
 			);
